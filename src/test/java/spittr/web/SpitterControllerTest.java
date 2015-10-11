26013e65bc34b04c6ee0ae31300691507527d025
Yu Wu <wuyucsu@gmail.com>
2015-10-11 17:15:43 -0500

    add test and implementation of ProcessRegistration handler

--- src/test/java/spittr/web/SpitterControllerTest.java
+++ src/test/java/spittr/web/SpitterControllerTest.java
@@ -1,18 +1,55 @@
 package spittr.web;
 
-import org.junit.Test;
-import org.springframework.test.web.servlet.MockMvc;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.standaloneSetup;
+
+import org.junit.Test;
+import org.springframework.test.web.servlet.MockMvc;
+
+import spittr.Spitter;
+import spittr.data.SpitterRepository;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+import spittr.web.SpitterController;
 
 public class SpitterControllerTest {
 
 	@Test
 	public void shouldShowRegistration() throws Exception {
-		SpitterController controller = new SpitterController();
+		SpitterRepository mockRepository = mock(SpitterRepository.class);
+		SpitterController controller = new SpitterController(mockRepository);
 		MockMvc mockMvc = standaloneSetup(controller).build();
 		mockMvc.perform(get("/spitter/register")).andExpect(view().name("registerForm"));
+	}
+
+	@Test
+	public void shouldProcessRegistration() throws Exception {
+		SpitterRepository mockRepository = mock(SpitterRepository.class);
+		Spitter unsaved = new Spitter("jbauer", "24hours", "Jack", "Bauer", "jbauer@ctu.gov");
+		Spitter saved = new Spitter(24L, "jbauer", "24hours", "Jack", "Bauer", "jbauer@ctu.gov");
+		when(mockRepository.save(unsaved)).thenReturn(saved);
+
+		SpitterController controller = new SpitterController(mockRepository);
+		MockMvc mockMvc = standaloneSetup(controller).build();
+
+		mockMvc.perform(post("/spitter/register").param("firstName", "Jack").param("lastName", "Bauer")
+				.param("username", "jbauer").param("password", "24hours").param("email", "jbauer@ctu.gov"))
+				.andExpect(redirectedUrl("/spitter/jbauer"));
+		/*
+		 * When handling a POST request, it¡¯s usually a good idea to send a
+		 * redirect after the POST has completed processing so that a browser
+		 * refresh won¡¯t accidentally submit the form a second time.
+		 */
+
+		verify(mockRepository, atLeastOnce()).save(unsaved);
+		/*
+		 * Finally, the test verifies that the mocked SpitterRepositorywas
+		 * actually used to save the data coming in on the form
+		 */
 	}
 
 }
